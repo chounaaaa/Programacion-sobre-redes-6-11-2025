4f145402861476d22403762ce008c0b665d9bbf5
chounaaaa <sunavij2@gmail.com>
2025-10-17 17:43:58 -0300

    se añadieron archivos

---
+++ EjercicioLoginWindows/app/src/main/java/com/example/ejerciciologinwindows/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ejerciciologinwindows
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Ajustar los bordes
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //  Vínculos con los elementos del XML (correo contraseña etc)
+        val correoTB = findViewById<EditText>(R.id.TextBox_Correo)
+        val passwordTB = findViewById<EditText>(R.id.TextBox_contraseña)
+        val botonLogin = findViewById<Button>(R.id.login_button)
+
+        //  Evento al presionar el botón
+        botonLogin.setOnClickListener {
+            val correo = correoTB.text.toString()
+            val password = passwordTB.text.toString()
+
+            //  Condicion
+            if (correo == "sunavij2@gmail.com" && password == "123456") {
+                Toast.makeText(this, "Inicio de sesión correcto", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Error, datos incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
